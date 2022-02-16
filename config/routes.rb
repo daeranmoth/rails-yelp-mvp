d748a4a7582fa4042429a71dc82fed6db2c22afb
gary camboulives <gary.camboulives@gmail.com>
2022-02-16 14:08:19 +0100

    rake good

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:new, :create, :show, :index] do
+    # collection do
+    #   get :top
+    # end
+    # member do
+    #   get :contact
+    # end
+    resources :reviews, only: [:new, :create]
+  end
+
+  # ressources :reviews, only: [:destroy]
 end
